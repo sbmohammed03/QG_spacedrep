b1dc7b5d3a4ea921bfd30dd7b7ed03ace3fc8e40
MukundhMurthy <56777001+MukundhMurthy@users.noreply.github.com>
2020-10-04 14:50:00 -0400

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -3,7 +3,28 @@ import numpy as np
 import pandas as pd
 import webbrowser
 from bokeh.models.widgets import Div
+import spacy
+from transformers import AutoTokenizer, AutoModelWithLMHead
 
+nlp = spacy.load('en_core_web_sm')
+doc = nlp(input_context)
+tokenizer = AutoTokenizer.from_pretrained("mrm8488/t5-base-finetuned-question-generation-ap")
+model = AutoModelWithLMHead.from_pretrained("mrm8488/t5-base-finetuned-question-generation-ap")
+
+def get_question(answer, context, max_length=64):
+    input_text = "answer: %s  context: %s </s>" % (answer, context)
+    features = tokenizer([input_text], return_tensors='pt')
+    output = model.generate(input_ids=features['input_ids'], 
+    attention_mask=features['attention_mask'],
+    max_length=max_length)
+    return tokenizer.decode(output[0])
+
+def return_questions(context):
+    questions = []
+    for ent in doc.ents:
+        questions.append(get_question(ents.text, context))
+    st.write(questions)
+    
 st.title('Our Awesome Name')
 st.header('There are three main methods to generate flashcards.')
 
@@ -21,7 +42,10 @@ if st.button('Download Browser Extension'):
 st.subheader('Method 2:')
 st.write('Feel free to copy and paste your text.')
 sentence = st.text_input('Paste Text Here:') 
-
+subm = st.button('Submit context')
+if subm:
+    st.write(return_questions(sentence))
+    
 #Method3
 st.subheader('Method 3:')
 st.write('You can also upload files')
